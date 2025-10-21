38f785ccb40f5122fb82db23f5bfd021a0249f1c
Pauline <plinekaeorg@gmail.com>
2025-10-21 11:44:38 +0200

    added a User class

---
+++ QuestTracker/QuestTracker/User.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HeroProject
+
+{
+    //User klass
+    public class User
+    {
+
+        public string Username;
+        public string Password;
+        public string PhoneNumber;
+
+        public void SetInfo(string username, string password, string phonenumber)
+        {
+            Username = username;
+            Password = password;
+            PhoneNumber = phonenumber;
+        }
+
+
+        public void ShowProfile()
+        {
+            Console.WriteLine($"Profile: {Username} - {PhoneNumber}");
+        }
+    }
+}
