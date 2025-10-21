986188129b7884002592cc13fda1c2ccf0b946d0
Pauline <plinekaeorg@gmail.com>
2025-10-21 18:42:05 +0200

    Thought it would be fun to have an AI class but its not done yet. Took the time to write some code in the main.

--- QuestTracker/QuestTracker/Program.cs
+++ QuestTracker/QuestTracker/Program.cs
@@ -1,10 +1,22 @@
-﻿namespace QuestTracker
+﻿using HeroProject;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+
+internal class Program
 {
-    internal class Program
+
+
+    private static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            
-        }
+
+        Console.Title = "Quest Guild Tracker";
+        Console.ForegroundColor = ConsoleColor.Green;
+
+        // Startar huvudmenyn från MenuHelper-klassen
+        MenuHelper.StartMenu();
+
+        Console.ResetColor();
     }
+
 }
